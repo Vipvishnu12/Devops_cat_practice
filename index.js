1749d25fdee44791d641536fdfb8df6d0ad6c8ac
Vishnu <23cs066@nandhaengg.org>
2026-05-14 21:45:42 +0530

    initial push

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("AWS Demo Backend Running 🚀");
+});
+
+app.get("/api", (req, res) => {
+    res.json({
+        success: true,
+        message: "Hello from backend"
+    });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
