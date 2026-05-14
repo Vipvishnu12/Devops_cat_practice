8c3768266de0f5b286de788f4c49de14cb62b5bf
Vishnu <23cs066@nandhaengg.org>
2026-05-14 23:31:48 +0530

    eeferf

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get("/api", (req, res) => {
 const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server runnrrghing on port ${PORT}`);
+    console.log(`Server erereere on port ${PORT}`);
 });
